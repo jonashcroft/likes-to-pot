b57ce7cc821e8d6f055e8cbd13970d73d0e19571
Jon Ashcroft <hello@jonashcroft.co.uk>
2018-09-15 19:49:49 +0100

    Fix: double quotes in log not creating new line break 🤯 && git push

--- public/functions/createLog.php
+++ public/functions/createLog.php
@@ -5,6 +5,6 @@ function createLog( $logMessage ) {
     $logFilePath = 'logs/cron-log.txt';
     $fullLog     = '[' . date('Y-m-d H:m:s') . '] ' . $logMessage;
 
-    file_put_contents( $logFilePath, $fullLog . '\n', FILE_APPEND );
+    file_put_contents( $logFilePath, $fullLog . "\n", FILE_APPEND );
 
 }
